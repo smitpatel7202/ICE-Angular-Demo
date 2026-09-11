61e0bd1e221d79e8d0c2b0943a1ed69815ffeb33
Smit Patel <2202031000118@silveroakuni.ac.in>
2026-09-11 23:23:26 +0530

    Fix duplicate io identifier syntax error in server.js

--- server.js
+++ server.js
@@ -4,27 +4,30 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+
+// Single 'io' declaration with CORS enabled
 const io = new Server(server, {
-  cors: { origin: '*' }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
+
   socket.on('chat message', (msg) => {
-    // Log and broadcast the message to all other clients
     console.log('received chat message from', socket.id, msg);
+    // Broadcast message to all other connected clients
     socket.broadcast.emit('chat message', msg);
   });
+
   socket.on('disconnect', () => {
     console.log('user disconnected', socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
-
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Allows connections from Vercel domain
-    methods: ["GET", "POST"]
-  }
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
 });
